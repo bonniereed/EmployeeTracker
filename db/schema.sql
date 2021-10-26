3c474d4e1bff2c39dce6c9a3aff2d0596828effe
Bonnie Reed <bonniejeanreed@gmail.com>
2021-10-26 14:25:13 -0500

    init commit

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS workplace_db;
+CREATE DATABASE workplace_db;
+
+USE workplace_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE emp_role (
+    id INT PRIMARY KEY,
+    emp_role VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    FOREIGN KEY (departments_id),
+    REFERENCES department(id),
+);
+
+CREATE TABLE employee(
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  FOREIGN KEY (roles_id),
+  REFERENCES emp_role(id),
+  
+)
+
+--You might want to use a separate file that contains 
+--functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these. You might also want to include a seeds.sql file to pre-populate your database, making the 
+--development of individual features much easier.
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (department_name)
+VALUES ( "Engineering",),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO emp_role (emp_role,salary)
+VALUES ("Sales Lead", 100000),
+       ("Sales Person", 80000),
+        ("Lead Engineer"150000),
+        ("Software Engineer"120,000),
+        ("Account Manager"160000),
+        ("Legal Team Lead", 250000),
+        ("Lawyer",190000);
+       
+INSERT INTO employee (first_name, last_name,)
