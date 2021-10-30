03717efe3de3ba6cb27655fb6c1051be184b5f08
Bonnie Reed <bonniejeanreed@gmail.com>
2021-10-29 19:56:59 -0500

    fixed syntax errors in schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -2,27 +2,26 @@ DROP DATABASE IF EXISTS workplace_db;
 CREATE DATABASE workplace_db;
 
 USE workplace_db;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS emp_role;
+DROP TABLE IF EXISTS employee;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
-);
-USE workplace_db;
-CREATE TABLE emp_role (
-    id INT PRIMARY KEY,
-    emp_role VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    FOREIGN KEY (departments_id),
+    department_name VARCHAR(30) NOT NULL
 );
 
-USE workplace_db;
+CREATE TABLE emp_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    emp_role VARCHAR(30) NOT NULL,
+    salary INT,
+    department_id INT
+);
+
+
 CREATE TABLE employee(
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   manager_id INT,
-  FOREIGN KEY (roles_id),
+  role_id INT
 );
-
---You might want to use a separate file that contains 
---functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these. You might also want to include a seeds.sql file to pre-populate your database, making the 
---development of individual features much easier.
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-SET NAMES 'utf8'
-SET CHARACTER SET utf8
-
 INSERT INTO department (department_name)
 VALUES ( "Engineering"),
        ("Finance"),
@@ -10,10 +7,8 @@ VALUES ( "Engineering"),
 INSERT INTO emp_role (emp_role, salary)
 VALUES ("Sales Lead", 100000),
        ("Sales Person", 80000),
-        ("Lead Engineer"150000),
-        ("Software Engineer"120000),
-        ("Account Manager"160000),
+        ("Lead Engineer", 150000),
+        ("Software Engineer",120000),
+        ("Account Manager",160000),
         ("Legal Team Lead", 250000),
         ("Lawyer",190000);
-
-INSERT INTO employee (first_name, last_name,)
